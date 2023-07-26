38de5f241db3b2d94748a80b6cdfb253e684f333
MNafisN <naf1s.n4ufally@gmail.com>
2023-07-26 17:03:03 +0700

    kerangka Service Repository Pattern untuk JWT auth

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+
+        \App\Models\User::create([
+            'username' => 'test1234',
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ]);
     }
 }
